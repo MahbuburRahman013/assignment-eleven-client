f33e79fab468d53468ebc4b0bc91519bc4e7cd3e
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-11-05 15:03:21 +0600

    added tow button login and registration

--- src/logo/Logo.jsx
+++ src/logo/Logo.jsx
@@ -1,13 +1,25 @@
+import { Link } from "react-router-dom";
 
 
 const Logo = () => {
     return (
-        <div className=" bg-gray-800">
-            <div className="container mx-auto py-2 flex items-center border-b-2 border-red-800">
-            <p className="uppercase text-3xl pl-5 font-bold text-[#7495b4]">Study<span className="text-[#f9af32]">Hub</span></p>
-            <img className="w-20" src="https://i.ibb.co/wrq6wjT/181620731-online-course-with-small-group-coaching-for-study-skills-logo-design-1-removebg-preview.png" alt="" />
-            
-        </div>
+        <div className=" bg-gray-800 border-b-2 border-red-800">
+            <div className="container mx-auto py-2 justify-between flex items-center">
+                <div className="flex">
+                    <p className="uppercase text-3xl pl-5 font-bold text-[#7495b4]">Study<span className="text-[#f9af32]">Hub</span></p>
+                    <img className="w-20" src="https://i.ibb.co/wrq6wjT/181620731-online-course-with-small-group-coaching-for-study-skills-logo-design-1-removebg-preview.png" alt="" />
+                </div>
+                <div className="text-white">
+                    <Link to='/registration' className="inline-block px-4 py-1 text-white bg-blue-500 rounded hover:bg-blue-700 transition duration-300 ease-in-out">
+                        Registration
+                    </Link>
+                    <Link to='/login' className="inline-block mx-5 px-4 py-1 text-white bg-[#f9af32] rounded hover:bg-orange-700 transition duration-300 ease-in-out">
+                       Login
+                    </Link>
+                    
+                </div>
+            </div>
+
         </div>
     );
 };
